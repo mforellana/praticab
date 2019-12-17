272e53b880fba2438161a87ef8f642f249033a50
SONY <SONY@DESKTOP-FPFEB6M.mshome.net>
2019-12-17 08:00:57 -0500

    proyecto para practica

---
+++ src/practicab/usapractica.java
@@ -0,0 +1,11 @@
+
+package practicab;
+
+
+public class usapractica {
+    public static void main(String[] args) {
+        practica p=new practica();
+        p.setVisible(true);
+        p.setLocationRelativeTo(null);
+    }
+}
