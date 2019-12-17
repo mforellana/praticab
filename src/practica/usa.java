2590af9f25e3b7321d8258c5e5b7988daf507fab
luis guillermo <luisguillermo0220@gmail.com>
2019-12-17 07:52:47 -0500

    Hola amigos

---
+++ src/practica/usa.java
@@ -0,0 +1,12 @@
+
+package practica;
+
+
+public class usa {
+   public static void main(String args ){
+       practica p=new practica();
+         p.setVisible(true);
+         p.setLocationRelativeTo(null);
+   }
+}
+
